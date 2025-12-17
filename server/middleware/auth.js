955ba97112a0ee1312cef8caeb804dd1ba7a6fc7
himanshumaurya45 <himanshumaurya0007@gmail.com>
2025-12-17 20:51:52 +0530

    commit after creating all the apis for performing the task like generating and creating

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -3,8 +3,23 @@ import { clerkClient } from "@clerk/express";
 export const auth = async (req,res,next)=> {
     try {
         const {userId,has}=req.auth();
-        const hasPremiumedPlan=await has({plan:"premium"});
+        const hasPremiumPlan=await has({plan:"premium"});
+        const user=await clerkClient.users.getUser(userId);
+
+        if(!hasPremiumPlan && user.privateMetadata.free_usage){
+            req.free_usage=user.privateMetadata.free_usage;
+        }
+        else{
+            await clerkClient.users.updateUserMetadata(userId,{
+                privateMetadata:{
+                    free_usage:0
+                }
+            });
+            req.free_usage=0;
+            next();
+        }
+        req.plan=hasPremiumPlan?"premium":"free";
     } catch (error) {
-        
+        res.json({success:false,message:error.message});
     }
 }
